1d09bf1b05bc288b4e1b39eab42be8ff926d5568
LeeJusuk208 <leejusuk208@gmail.com>
2022-08-19 14:28:13 +0900

    Modify Storage Engine Instance for Changed Snippet Protobuf

--- KETI-Query-Process-Container/gRPC_sample/storage_engine_instance.cc
+++ KETI-Query-Process-Container/gRPC_sample/storage_engine_instance.cc
@@ -25,22 +25,253 @@ class SnippetSampleServiceImpl final : public SnippetSample::Service {
                    ServerReaderWriter<Result, Snippet>* stream) override {
     Snippet snippet;
     while (stream->Read(&snippet)) {
-      std::cout << "SetSnippet" << std::endl;
-      std::cout << "queryid :" << snippet.queryid() << std::endl;
-      std::cout << "workid :" << snippet.workid() << std::endl;
-      std::cout << "snippet :" << snippet.snippet() << std::endl << std::endl;
-
+      std::cout << "Recv Snippet" << std::endl;
+      
+  // QueryID
+  {
+    std::cout << "\"QueryID\" : " << snippet.query_id() << std::endl;
+  }
+  
+  // WorkID
+  {
+    std::cout << "\"WorkID\" : " << snippet.work_id() << std::endl;
+  }
+  
+  // table_name
+  {
+    std::cout << "\"table_name\" : [";
+    for(int i=0;i<snippet.table_name_size();i++){
+        std::cout << "\"" << snippet.table_name(i) << "\",";
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // table_col
+  {
+    std::cout << "\"table_col\" : [" << std::endl;
+    for(int i=0;i<snippet.table_col_size();i++){
+        std::cout << "\t\"" << snippet.table_col(i) << "\"," << std::endl;
+    }
+    std::cout << "]," << std::endl;
+  }
     
-      {
-        query_result += "w_id :";
-        query_result += std::to_string(snippet.workid());    
-        query_result += "snippet str :";
-        query_result += snippet.snippet();
-        query_result += "\n";
-      }
+  // table_filter
+  {
+    std::cout << "\"table_filter\" : [" << std::endl;
+    for(int i=0;i<snippet.table_filter_size();i++){
+        auto filter = snippet.table_filter(i);
+        std::cout << "\t{";
+        switch(filter.operator_()){
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_GE:
+            std::cout << "\"OPERATOR\" : OperType_KETI_GE,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_LE:
+            std::cout << "\"OPERATOR\" : OperType_KETI_LE,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_GT:
+            std::cout << "\"OPERATOR\" : OperType_KETI_GT,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_LT:
+            std::cout << "\"OPERATOR\" : OperType_KETI_LT,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_ET:
+            std::cout << "\"OPERATOR\" : OperType_KETI_ET,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_NE:
+            std::cout << "\"OPERATOR\" : OperType_KETI_NE,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_LIKE:
+            std::cout << "\"OPERATOR\" : OperType_KETI_LIKE,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_BETWEEN:
+            std::cout << "\"OPERATOR\" : OperType_KETI_BETWEEN,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_IN:
+            std::cout << "\"OPERATOR\" : OperType_KETI_IN,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_IS:
+            std::cout << "\"OPERATOR\" : OperType_KETI_IS,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_ISNOT:
+            std::cout << "\"OPERATOR\" : OperType_KETI_LE,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_NOT:
+            std::cout << "\"OPERATOR\" : OperType_KETI_NOT,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_AND:
+            std::cout << "\"OPERATOR\" : OperType_KETI_AND,";
+            break;
+            case typetest::Request_Filter_OperType::Request_Filter_OperType_KETI_OR:
+            std::cout << "\"OPERATOR\" : OperType_KETI_LE,";
+            break;
+        }
+        for(int j=0;j<filter.sany_size();j++){
+            const typetest::Request_SnippetAny& any = filter.sany(j);
+            if(j==0){
+                std::cout << "\"LV\" : ";
+            } else if(j==1){
+                std::cout << "\"RV\" : ";
+            } else {
+                std::cout << "\"EXTRA\" : ";
+            }
+            
+            switch(any.snippettype()){
+                case typetest::Request_ValueType::Request_ValueType_INT32:
+                    int ibuf;
+                    memcpy(&ibuf, any.value().c_str(), any.value().size());
+                    std::cout << "ValueType_INT32 : " << ibuf << ",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_NUMERIC:
+                    std::cout << "ValueType_NUMERIC : \"" << any.value() << "\",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_STRING:
+                    std::cout << "ValueType_STRING : \"" << any.value() << "\",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_DATE:
+                    int dtbuf;
+                    memcpy(&dtbuf, any.value().c_str(), any.value().size());
+                    std::cout << "ValueType_DATE : " << dtbuf << ",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_COLUMN_NAME:
+                    std::cout << "COLUMN_NAME : \"" << any.value() << "\",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_OPERATOR:
+                    std::cout << "ValueType_STRING : \"" << any.value() << "\",";
+                break;
+            }
+        }
+        std::cout << "}," << std::endl;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // table_offset
+  {
+    std::cout << "\"table_offset\" : [";
+    for(int i=0;i<snippet.table_offset_size();i++){
+        std::cout << snippet.table_offset(i) << ",";
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // table_offlen
+  {
+    std::cout << "\"table_offlen\" : [";
+    for(int i=0;i<snippet.table_offlen_size();i++){
+        std::cout << snippet.table_offlen(i) << ",";;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // table_datatype
+  {
+    std::cout << "\"table_datatype\" : [";
+    for(int i=0;i<snippet.table_datatype_size();i++){
+        std::cout << snippet.table_datatype(i) << ",";;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // BlockList
+  {
+    std::cout << "\"BlockList\" : []," << std::endl;
+  }
+  
+  // table_alias
+  {
+    std::cout << "\"table_alias\" : \"" << snippet.table_alias() << "\"" << std::endl;
+  }
+  
+  // column_alias
+  {
+    std::cout << "\"column_alias\" : [" << std::endl;
+    for(int i=0;i<snippet.column_alias_size();i++){
+        std::cout << "\t\"" << snippet.column_alias(i) << "\"," << std::endl;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // column_projection
+  {
+    std::cout << "\"column_projection\" : [" << std::endl;    
+    for(int i=0;i<snippet.column_projection_size();i++){
+        auto projection = snippet.column_projection(i);
+        std::cout << "\t{";        
+        switch(projection.projcetiontype()){
+            case typetest::Request_Projection_ProjectionType::Request_Projection_ProjectionType_DEFAULT:
+            std::cout << "0,";
+            break;
+            case typetest::Request_Projection_ProjectionType::Request_Projection_ProjectionType_SUM:
+            std::cout << "1,";
+            break;
+            case typetest::Request_Projection_ProjectionType::Request_Projection_ProjectionType_AVG:            
+            std::cout << "2,";
+            break;
+            case typetest::Request_Projection_ProjectionType::Request_Projection_ProjectionType_COUNT:            
+            std::cout << "4,";
+            break;
+        }
+        for(int j=0;j<projection.sany_size();j++){
+            const typetest::Request_SnippetAny& any = projection.sany(j);
+            switch(any.snippettype()){
+                case typetest::Request_ValueType::Request_ValueType_INT32:
+                    int ibuf;
+                    memcpy(&ibuf, any.value().c_str(), any.value().size());
+                    std::cout << ibuf << ",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_NUMERIC:
+                break;
+                case typetest::Request_ValueType::Request_ValueType_STRING:
+                    std::cout << "\"" << any.value() << "\",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_COLUMN_NAME:
+                    std::cout << "\"" << any.value() << "\",";
+                break;
+                case typetest::Request_ValueType::Request_ValueType_OPERATOR:
+                    std::cout << "\"" << any.value() << "\",";
+                break;
+            }
+        }
+        std::cout << "}," << std::endl;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // column_filtering
+  {
+    std::cout << "\"column_filtering\" : [" << std::endl;
+    for(int i=0;i<snippet.column_filtering_size();i++){
+        std::cout << "\t\"" << snippet.column_filtering(i) << "\"," << std::endl;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // Group by
+  {
+    std::cout << "\"Group by\" : [" << std::endl;
+    for(int i=0;i<snippet.group_by_size();i++){
+        std::cout << "\t\"" << snippet.group_by(i) << "\"," << std::endl;
+    }
+    std::cout << "]," << std::endl;
+  }
+  
+  // Order by
+  {
+    std::cout << "\"Order by\" : [" << std::endl;
+    for(int i=0;i<snippet.order_by_size();i++){        
+        auto odr = snippet.order_by(i);
+        std::cout << "\t{";
+        if(odr.direction() == typetest::Request_Order_OrderDirection::Request_Order_OrderDirection_DESC){
+            std::cout << "1,";
+        }
+        std::cout << "\"" << odr.column() << "\"}," << std::endl;
+    }
+    std::cout << "]" << std::endl;
+  }
 
       if(snippet.snippet() == "return"){
         Result result;
+        query_result = "Under Construct";
         result.set_value(query_result);
         stream->Write(result);
       }
@@ -50,6 +281,8 @@ class SnippetSampleServiceImpl final : public SnippetSample::Service {
   Status Run(ServerContext* context, const Request* request, Result* result) override {
     std::cout << "Run" << std::endl;
     std::cout << "req queryid :" << request->queryid() << std::endl << std::endl;
+        
+    query_result = "Under Construct";
 
     result->set_value(query_result);
 
